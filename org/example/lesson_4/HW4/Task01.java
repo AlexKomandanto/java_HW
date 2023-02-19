a6a4998ecc6da8e112a6b21c0fa02c4e5855dcfc
AlexKomandanto <komandanto666@gmail.com>
2023-02-19 22:57:29 +0300

    Add new HW4_01

--- org/example/lesson_4/HW4/Task01.java
+++ org/example/lesson_4/HW4/Task01.java
@@ -4,30 +4,30 @@ package lesson_4.HW4;
 
  */
 
-import java.util.ArrayDeque;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Scanner;
+import java.util.*;
 
 public class Task01 {
     public static void main(String[] args) {
-        //Scanner sc = new Scanner(System.in);
         LinkedList<String> list = new LinkedList<>();
-        //descendingIterator(list);
-        list.add("abcd");
-        list.add("efgh");
-        list.add("lkmn");
-        System.out.println(list);
+        list.add("1234");
+        list.add("5678");
+        list.add("90-=");
+        System.out.println("LinkedList:" + list);
         Iterator a = list.descendingIterator();  // создали итератор
 
-        LinkedList<String> listRevers = new LinkedList<>();
-
         while (a.hasNext()) { // вывод массива с помощью итератора
-            //System.out.println(a.next());
-            //LinkedList<String> listRevers = new LinkedList<>();
-            listRevers.addFirst((String) a.next());
-            //System.out.println(listRevers);
+            System.out.println("Значение : " + a.next());
         }
-        System.out.println(listRevers);
+
+        // 2-й вариант решения через коллекции
+//        LinkedList<String> list = new LinkedList<>();
+//        list.add("one");
+//        list.add("two");
+//        list.add("three");
+//        System.out.println(list);
+//
+//        Collections.reverse(list);
+//
+//        System.out.println(list);
     }
 }
--- org/example/lesson_5/HW5/Task01.java
+++ org/example/lesson_5/HW5/Task01.java
@@ -12,13 +12,21 @@ public class Task01 {
 
     }
 
-    public static void createTelBook (Integer id, String numerical){
-        Map<String, ArrayList<String>> telBook = new HashMap<String, ArrayList<String>>();
+//    public static void createTelBook (Integer id, String numerical){
+//        //Map<String, ArrayList<String>> phoneBook = new HashMap<String, ArrayList<String>>();
+//
+//        //phoneBook.put("Иванов", createArray());
+//        HashMap<String, List<String>> phoneBook = new HashMap<>();
+//
+//// Add a person with a single number
+//        phoneBook.put("John Doe", Arrays.asList("123-456-7890"));
+//
+//// Add a person with multiple numbers
+//        phoneBook.put("Jane Doe", Arrays.asList("123-456-7890", "098-765-4321"));
+//        phoneBook.put("Иванов", createArray());
+//    }
 
-        //telBook.put("Иванов", createArray.toString());
-    }
-
-    public String[] createArray() {
+    public List<String> createArray() {
         Scanner sc = new Scanner(System.in);
         int size = 3;
         String[] myArray = new String[size];
@@ -26,9 +34,25 @@ public class Task01 {
         for(int i=0; i<size; i++) {
             myArray[i] = sc.nextLine();
         }
-        return myArray;
+        return List.of(myArray);
     }
 
+    HashMap<String, List<Integer>> phoneBook = new HashMap<>();
+    // Add a person to the phone book
+    public void addPerson(String name) {
+        List<Integer> phones = new ArrayList<>();
+        int randomNum = (int) (Math.random() * 100);
+        phones.add(randomNum);
+        phoneBook.put(name, phones);
+    }
+
+    // Add a phone number to an existing person
+    public void addPhone(String name, int phoneNum) {
+        if (phoneBook.containsKey(name)) {
+            List<Integer> phones = phoneBook.get(name);
+            phones.add(phoneNum);
+        }
+    }
 //    static void printResult1(String a){
 //        System.out.println(String.format(
 //                "%s isPlaisment -> %s", a, isPlaisment(a) ? "true" : "false"));
