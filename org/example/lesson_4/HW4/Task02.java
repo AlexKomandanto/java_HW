ddfaca534898f866d459bf1f32599215c820e7d4
AlexKomandanto <komandanto666@gmail.com>
2023-02-19 23:13:17 +0300

    Add new HW4_02

--- org/example/lesson_4/HW4/Task02.java
+++ org/example/lesson_4/HW4/Task02.java
@@ -1,36 +1,42 @@
 package lesson_4.HW4;
 
-import java.util.ArrayDeque;
-import java.util.Scanner;
+import java.util.LinkedList;
 
 /*
-    Реализуйте очередь с помощью LinkedList со следующими методами:enqueue()
-- помещает элемент в конец очереди, dequeue()
-- возвращает первый элемент из очереди и удаляет его, first()
-- возвращает первый элемент из очереди, не удаляя.
+    Реализуйте очередь с помощью LinkedList со следующими методами:
+    enqueue()- помещает элемент в конец очереди,
+    dequeue()- возвращает первый элемент из очереди и удаляет его,
+    first()- возвращает первый элемент из очереди, не удаляя.
  */
 public class Task02 {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        ArrayDeque<String> deque = new ArrayDeque<>();
+    }
+    private LinkedList<Object> list;
 
-        while(true) {
-            System.out.println("Введите данные в форме text~num: ");
-            String input = sc.nextLine(); // ввод данных пользователем
+    public void LinkedQueue() {
+        list = new LinkedList<Object>();
+    }
 
-            if(input.equals("q"))break;
+    public void enqueue(Object element) {
+        list.addLast(element);
+    }
 
-            else if(input.equals("print")){
-                System.out.println(deque);  // выводит перевернутый массив, как в условии
-            } else if(input.equals("revert")){
-                System.out.println(deque.pollFirst()); // удаляет последний доб элем
-            } else{
-                deque.addFirst(input); // добавляет введеное значение в начало
-                // (такое добавление усложняет функцию)
-            }
-            System.out.println(deque);
+    public Object dequeue() {
+        if (list.isEmpty()) {
+            return null;
         }
-        sc.close();
+
+        Object element = list.getFirst();
+        list.removeFirst();
+        return element;
+    }
+
+    public Object first() {
+        if (list.isEmpty()) {
+            return null;
+        }
+
+        return list.getFirst();
     }
 }
